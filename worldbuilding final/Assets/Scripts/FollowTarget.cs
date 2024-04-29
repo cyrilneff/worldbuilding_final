10066a083ecda30927ae32ad0e5bbc3effd0f2aa
cyrilneff <clneff@andrew.cmu.edu>
2024-04-29 15:57:24 -0400

    Slight Camera Movement Rework

--- worldbuilding final/Assets/Scripts/FollowTarget.cs
+++ worldbuilding final/Assets/Scripts/FollowTarget.cs
@@ -5,30 +5,50 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
-    public Transform target;
- 
-    private Vector3 offset;
+    [SerializeField]
+    private Transform target;
 
-    //Some Input
-    //the players position
-    //Some Logic ("WHEN")
-    //always be following
-    //Some Output
-    //the cameras position
+    [SerializeField]
+    private Vector3 offsetPosition;
 
-    private void Start()
-    {
-        if (target == null)
-        {
-            Debug.LogWarning(message: "No Target for script.", context: this);
-        }
-        offset = transform.position - target.position;
-    }
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
 
     private void Update()
     {
-        //move our position to player position
-        transform.position = target.position + offset;
-        transform.LookAt(target.position);
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if(target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if(offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+
+        // compute rotation
+        if(lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
     }
 }
